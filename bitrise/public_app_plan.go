e25b9aac2465dc4b69d3ae6b5a424331a8acc477
whage <whage@freemail.hu>
2020-05-07 19:21:41 +0200

    Add error handling to setter methods of PublicAppPlan

--- bitrise/public_app_plan.go
+++ bitrise/public_app_plan.go
@@ -1,5 +1,9 @@
 package bitrise
 
+import (
+	"fmt"
+)
+
 const publicAppPlanDefaultConcurrentBuildCount = 2
 const publicAppPlanDefaultMaximumBuildDurationInMinutes = 45
 
@@ -31,14 +35,18 @@ func (pap PublicAppPlan) GetMaximumTeamMembers() (bool, int) {
 	return false, 0
 }
 
-func (pap *PublicAppPlan) SetConcurrentBuildCount(n int) {
-	if n > publicAppPlanDefaultConcurrentBuildCount {
+func (pap *PublicAppPlan) SetConcurrentBuildCount(n int) (bool, error) {
+	if n >= publicAppPlanDefaultConcurrentBuildCount {
 		pap.concurrentBuildCount = n
+		return true, nil
 	}
+	return false, fmt.Errorf("New value (%d) is less than plan default (%d)", n, publicAppPlanDefaultConcurrentBuildCount)
 }
 
-func (pap *PublicAppPlan) SetMaximumBuildDurationInMinutes(n int) {
-	if n > publicAppPlanDefaultMaximumBuildDurationInMinutes {
+func (pap *PublicAppPlan) SetMaximumBuildDurationInMinutes(n int) (bool, error) {
+	if n >= publicAppPlanDefaultMaximumBuildDurationInMinutes {
 		pap.maximumBuildDurationInMinutes = n
+		return true, nil
 	}
+	return false, fmt.Errorf("New value (%d) is less than plan default (%d)", n, publicAppPlanDefaultMaximumBuildDurationInMinutes)
 }
