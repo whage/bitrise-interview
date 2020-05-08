36607e87fdf4eef0dc161843df814845cb718bd1
whage <whage@freemail.hu>
2020-05-08 22:13:18 +0200

    PublicApp type was full of mistakes after copying

--- bitrise/public_app.go
+++ bitrise/public_app.go
@@ -5,26 +5,28 @@ type PublicApp struct {
 	usesPublicAppLimits bool
 }
 
-func NewPublicApp(owner User) App {
-	return App{
-		owner: owner,
+func NewPublicApp(owner User) PublicApp {
+	return PublicApp{
+		App: App{
+			owner: owner,
+			IPlan: NewPublicAppPlan(),
+		},
 		usesPublicAppLimits: true,
-		IPlan: NewPublicAppPlan(),
 	}
 }
 
-func (a *App) OptOutFromPublicAppLimits() {
+func (a *PublicApp) OptOutFromPublicAppLimits() {
 	a.usesPublicAppLimits = false
 }
 
-func (a *App) SetConcurrentBuildCount(n int) (bool, error) {
+func (a *PublicApp) SetConcurrentBuildCount(n int) (bool, error) {
 	plan := a.IPlan.(PublicAppPlan)
 	ok, err := plan.SetConcurrentBuildCount(n)
 	a.IPlan = plan
 	return ok, err
 }
 
-func (a *App) SetMaximumBuildDurationInMinutes(n int) (bool, error) {
+func (a *PublicApp) SetMaximumBuildDurationInMinutes(n int) (bool, error) {
 	plan := a.IPlan.(PublicAppPlan)
 	ok, err := plan.SetMaximumBuildDurationInMinutes(n)
 	a.IPlan = plan
